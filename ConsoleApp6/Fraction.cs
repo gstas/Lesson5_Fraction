d5cd945b4ef176cf271199c4bb5bfabc37243569
user6 <user6@SERVER>
2019-06-10 20:50:35 +0300

    add Reduction

---
+++ ConsoleApp6/Fraction.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp6
+{
+    class Fraction
+    {
+        int numerator;
+        int denominator;
+
+        public int Numerator { get => numerator; set => numerator = value; }
+        public int Denominator { get => denominator; set => denominator = value == 0 ? 1 : value; }
+
+        public Fraction()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        public Fraction(int numerator) : this()
+        {
+            Numerator = numerator;
+
+        }
+
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+
+        public Fraction Reverse()
+        {
+            //return $"{denominator}/{numerator}";
+            return new Fraction(denominator, numerator);
+        }
+
+        public static Fraction Sum(Fraction fr1, Fraction fr2)
+        {
+            int num = fr1.Numerator * fr2.Denominator + fr2.Numerator * fr1.denominator;
+            int denom = fr1.Denominator * fr2.Denominator;
+            return new Fraction(num, denom);
+        }
+
+        public static Fraction Multi(Fraction fr1, Fraction fr2)
+        {
+            int num = fr1.Numerator * fr2.Numerator;
+            int denom = fr1.Denominator * fr2.Denominator;
+            return new Fraction(num, denom);
+        }
+
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fr1 = new Fraction();
+            Console.WriteLine(fr1);
+            Console.WriteLine(fr1.Reverse());
+            Console.WriteLine();
+
+            Fraction fr2 = new Fraction(2);
+            Console.WriteLine(fr2);
+            Console.WriteLine(fr2.Reverse());
+            Console.WriteLine();
+
+            Fraction fr3 = new Fraction(2, 3);
+            Console.WriteLine(fr3);
+            Console.WriteLine(fr3.Reverse());
+
+            Console.WriteLine();
+            Console.WriteLine(Fraction.Sum(new Fraction(1,2), new Fraction(7,3)));
+
+        }
+    }
+}

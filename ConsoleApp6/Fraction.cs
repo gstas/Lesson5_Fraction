6d75257c259d8a81a5d1c9530cc1321dc9d9ce11
gstas <gstas07@gmail.com>
2019-06-10 23:43:45 +0300

    Add REduction, Normal, Denormal

--- ConsoleApp6/Fraction.cs
+++ ConsoleApp6/Fraction.cs
@@ -2,12 +2,14 @@
 {
     class Fraction
     {
+        int wholePart = 0;
         int numerator;
         int denominator;
 
         public int Numerator { get => numerator; set => numerator = value; }
         public int Denominator { get => denominator; set => denominator = value == 0 ? 1 : value; }
 
+        /*
         public Fraction()
         {
             Numerator = 1;
@@ -17,8 +19,8 @@
         public Fraction(int numerator) : this()
         {
             Numerator = numerator;
-
         }
+        */
 
         public Fraction(int numerator, int denominator)
         {
@@ -26,19 +28,27 @@
             Denominator = denominator;
         }
 
+        public Fraction(int wholePart, int numerator, int denominator):this(numerator, denominator)
+        {
+            this.wholePart = wholePart;
+        }
+
         public override string ToString()
         {
-            return $"{numerator}/{denominator}";
+            return $"{((wholePart == 0)?"":wholePart+"/")}{numerator}/{denominator}";
         }
 
         public Fraction Reverse()
         {
-            //return $"{denominator}/{numerator}";
+            DeNormal();
             return new Fraction(denominator, numerator);
         }
 
         public static Fraction Sum(Fraction fr1, Fraction fr2)
         {
+            fr1.DeNormal();
+            fr2.DeNormal();
+
             int num = fr1.Numerator * fr2.Denominator + fr2.Numerator * fr1.denominator;
             int denom = fr1.Denominator * fr2.Denominator;
             return new Fraction(num, denom);
@@ -46,9 +56,43 @@
 
         public static Fraction Multi(Fraction fr1, Fraction fr2)
         {
+            fr1.DeNormal();
+            fr2.DeNormal();
+
             int num = fr1.Numerator * fr2.Numerator;
             int denom = fr1.Denominator * fr2.Denominator;
             return new Fraction(num, denom);
+        }
+
+        public Fraction Reduction() {
+            DeNormal();
+
+            int min = Numerator == Denominator ? Numerator : 
+                            (Numerator < Denominator) ? Numerator : Denominator;
+
+            for (int i = min; i > 1 && Numerator > 1 && Denominator > 1 ; i--)
+			{
+                if (Numerator % i == 0 && Denominator % i == 0){
+                    Numerator /= i;
+                    Denominator /= i;
+                }                                                              
+			}
+
+            return new Fraction(Numerator, Denominator);
+        }
+
+        public Fraction Normal() {
+            wholePart = (int)Numerator/Denominator;
+            Numerator = Numerator - wholePart*Denominator;
+            return this;
+        }
+
+        public Fraction DeNormal() {
+            if (wholePart != 0) {
+                Numerator = wholePart*Denominator+Numerator;
+                wholePart = 0;
+            }
+            return this;
         }
 
     }
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
+            /*
             Fraction fr1 = new Fraction();
             Console.WriteLine(fr1);
             Console.WriteLine(fr1.Reverse());
@@ -15,14 +16,27 @@ namespace ConsoleApp6
             Console.WriteLine(fr2);
             Console.WriteLine(fr2.Reverse());
             Console.WriteLine();
+            */
 
-            Fraction fr3 = new Fraction(2, 3);
+            Fraction fr3 = new Fraction(6, 3);
             Console.WriteLine(fr3);
             Console.WriteLine(fr3.Reverse());
 
             Console.WriteLine();
             Console.WriteLine(Fraction.Sum(new Fraction(1,2), new Fraction(7,3)));
+            Console.WriteLine();
 
+            Console.WriteLine();
+            Console.WriteLine(Fraction.Multi(new Fraction(1,2), new Fraction(7,3)));
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine(new Fraction(84,36).Reduction());
+            Console.WriteLine();
+
+            Fraction fr4 = new Fraction(7, 2);    
+            Console.WriteLine(fr4.Normal());
+            Console.WriteLine(fr4.DeNormal());
         }
     }
 }
